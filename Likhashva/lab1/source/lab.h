39ec089bb8b90307b01b45d725c2fccd69b25b4f
NastyaLihashva <kassiopea308@mail.ru>
2021-03-21 22:27:04 +0300

    Likhashva_lab1
    Update

---
+++ Likhashva/lab1/source/lab.h
@@ -0,0 +1,31 @@
+﻿#ifndef LAB_H
+#define LAB_H
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+struct Square { //Квадрат, который можно расположить на столешнице
+	int size;
+	int x;
+	int y;
+
+};
+
+struct Table {
+	bool Field[50][50];
+	vector <Square> StepStack;  //Описывает заполнение столешницы
+	vector <Square> CurrentStack; //Текущее заполнение столешницы
+	vector <Square> BestStack; //Лучшее заполнение столешницы (наименьшее количество квадратов)
+
+	Square DeleteSquare(const int& size);
+	Square FreeCell(int size);
+	Square Place(int size, Square Alleged);
+	void Backtracking(int size);
+
+};
+
+
+#endif
