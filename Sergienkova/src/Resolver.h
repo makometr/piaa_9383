6190da694ea26f1ce006ddb3fcdc8b96ef1cf280
Anastasia_Sergienkova <onabila098@gmail.com>
2021-03-26 00:16:00 +0300

    opt.

--- Sergienkova/src/Resolver.h
+++ Sergienkova/src/Resolver.h
@@ -1,22 +1,22 @@
 #pragma once
 #include <iostream>
-#include <list>
+#include <vector>
 #include "Tile.h"
 
 class Resolver // решает задачу
 {
-	std::list<Tile> m_Result;	// результат работы
-	std::list<Tile> m_Tiles;	// список для перебора раскладок
+	std::vector<Tile> m_Result;	// результат работы
+	std::vector<Tile> m_Tiles;	// список для перебора раскладок
 	int m_N;					// размер стола
 	int m_Div;					// во сколько раз уменьшились при вычислениях результата
 	int** m_Data;				// двумерный массив занятых клеток
+	int m_SumS;					// суммарная полщадь
 	unsigned int m_Operations;	// количество операций при вичислении
 
 	bool CanAdd(Tile tile);		// возвращает истину, если в текущую раскладку можно добавить указанный тайл
 	void Add(Tile tile);		// добавляет в раскладку указанный тайл
 	void Remove();				// удаляет последний тайл из раскладки
 	bool Scan(Tile tile);		// попытка найти результат, начиная с добавления указанного тайла. Истина, если с этим тайлом скан возможен
-	bool HasResult();			// если истина, то получен результат
 	Position FindEmpty();		// возвращает первое попавшееся пустое место
 	void Resolve2();			// возвращает решение для четного n
 public:
