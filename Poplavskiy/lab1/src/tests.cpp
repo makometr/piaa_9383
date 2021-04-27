0617c72c87d2b0dbb722a72fc95bfd1860131712
didges <39410991+didges@users.noreply.github.com>
2021-04-27 17:06:26 +0300

    test update

--- Poplavskiy/lab1/src/tests.cpp
+++ Poplavskiy/lab1/src/tests.cpp
@@ -82,6 +82,41 @@ public:
         if (check) {
             std::cout << "Test 5 OK" << std::endl;
         }
+        else {
+            check = true;
+        }
+
+        try {
+            Square table(12);
+            table.insert_square(11, 13, 5, 1);
+        }
+        catch (...) {
+            std::cout << "Test 6 not OK" << std::endl;
+            check = false;
+        }
+        if (check) {
+            std::cout << "Test 6 OK" << std::endl;
+        }
+        else {
+            check = true;
+        }
+
+        try {
+            Square table(12);
+            table.insert_square(3, 3, 2, 1);
+            table.remove_square(14, 12, 3);
+        }
+        catch (...) {
+            std::cout << "Test 7 not OK" << std::endl;
+            check = false;
+        }
+        if (check) {
+            std::cout << "Test 7 OK" << std::endl;
+        }
+        else {
+            check = true;
+        }
+
     }
 
 };
