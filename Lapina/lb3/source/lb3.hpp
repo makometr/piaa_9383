ec65c40df0c90ed0c54cf41cf680959fc81f8179
Anastasialapina <anastasia.lap2001@mail.ru>
2021-05-08 12:26:38 +0300

    Lapina_lb3

---
+++ Lapina/lb3/source/lb3.hpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <map>
+#include <tuple>
+#include <vector>
+#include <algorithm>
+
+class edge {
+public:
+    edge(char v, float metrika){
+        this->v = v;
+        this->metrika = metrika;
+        this->view = 0;
+    }
+ 
+    char name()const{
+        return this->v;
+    }
+ 
+    float len()const{
+        return this->metrika;
+    }
+    void inc(int min){
+        this->metrika = this->metrika+min;
+    }
+
+    void dec(int min){
+        this->metrika = this->metrika-min;
+    }
+
+private:
+    char v;
+    float metrika;
+public:
+    int view;
+};
+
+class answ{
+    public:
+        answ(char source, char dest, int metrika){
+            this->source = source;
+            this->dest = dest;
+            this->metrika = metrika;
+        }
+ 
+    public:
+        char source;
+        char dest;
+        int metrika;
+};
+
+using Graph = std::map<char, std::vector<edge>>;
+
+int Search_min(std::string path, Graph G);
+void Read(Graph &G);
+void dec_metrika(Graph &G, std::string path, int min);
+void create_reverse_edges(Graph &G, std::string path, int min);
+std::string Search_path(Graph &G, char cur, char finish, std::string path);
+bool Check_v(Graph G, char start);
+std::vector <answ> for_answer(Graph G, Graph G2);
+int Algoritm(Graph &G, char start, char finish);
+
