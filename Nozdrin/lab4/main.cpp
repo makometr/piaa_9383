c03cc9ff906de81f79d7bf85f50dc0c85cdd0aea
BasilNozdrin <bassvass@gmail.com>
2021-09-27 22:10:29 +0300

    Nozdrin lab4 code done

--- Nozdrin/lab4/main.cpp
+++ Nozdrin/lab4/main.cpp
@@ -3,7 +3,7 @@
 
 #define KMP 1
 
-void print(std::vector<int>& numbers) {
+void print(std::vector<size_t> numbers) {
   for (int i = 0; i < numbers.size(); i++) {
     if (i == 0)
       std::cout << numbers[i];
@@ -13,59 +13,59 @@ void print(std::vector<int>& numbers) {
   std::cout << "\n";
 }
 
-std::vector<int> prefixFunction(const std::string& str) {
-  std::vector<int> prefixValues(str.length());
-  int counter = 0;
+std::vector<size_t> prefixFunction(const std::string& str) {
+  std::vector<size_t> prefixValues(str.length());
   for (int i = 1; i < str.size(); i++) {
-    if (str[counter] == str[i]) {
-      prefixValues[i] = ++counter;
-      continue;
-    }
-    if (counter == 0) {
-      prefixValues[i] = 0;
-      continue;
-    }
-    counter = prefixValues[counter-1];
-    i--;
+    size_t counter = prefixValues[i-1];
+    while (counter > 0 && str[counter] != str[i])
+      counter = prefixValues[counter-1];
+    if (str[i] == str[counter])
+      counter++;
+    prefixValues[i]=counter;
   }
   return prefixValues;
 }
 
-std::vector<int> kmp(std::string& sample, std::string& text) {
-  std::vector<int> result;
-  std::vector<int> samplePrefixValues = prefixFunction(sample);
-  int sampleInd = 0;
-  for (int textInd = 1; textInd < text.size(); textInd++) {
-    while ((sampleInd > 0) and (text[textInd] != sample[sampleInd+1])) {
-      sampleInd = samplePrefixValues[sampleInd];
-    }
-    if (text[textInd] == sample[sampleInd+1])
+std::vector<size_t> kmp(std::string& sample, std::string& text) {
+  std::vector<size_t> result;
+  std::vector<size_t> samplePrefixValues = prefixFunction(sample);
+  size_t sampleInd = 0;
+  for (size_t textInd = 0; textInd < text.size(); textInd++) {
+    if (text[textInd] == sample[sampleInd]) {
       sampleInd++;
-    if (sampleInd == sample.size()-1) {
-      result.push_back(textInd-(int)sample.size()+1);
-      sampleInd = samplePrefixValues[sampleInd];
+      if (sampleInd == sample.size()) {
+        result.push_back(textInd-sample.size()+1);
+      }
+      continue;
     }
+    if (sampleInd == 0) continue;
+    sampleInd = samplePrefixValues[sampleInd-1];
+    textInd--;
   }
-  if (result.empty()) {
+  if (result.empty())
     result.push_back(-1);
-    return result;
-  }
   return result;
 }
 
 int isCyclicShift(std::string& sample, std::string& text) {
-  if(sample.length()!=text.length())
+  if (sample.length()!=text.length())
     return -1;
-  std::vector<int> samplePrefixValues = prefixFunction(sample);
-  int sampleInd = 0;
-  for (int textInd = 1; textInd < 2*text.size(); textInd++) {
-    while ((sampleInd > 0) and (text[textInd%text.size()] != sample[sampleInd+1])) {
-      sampleInd = samplePrefixValues[sampleInd];
-    }
-    if (text[textInd%text.size()] == sample[sampleInd+1])
+  if (sample == text)
+    return 0;
+  std::vector<size_t> result;
+  std::vector<size_t> samplePrefixValues = prefixFunction(sample);
+  size_t sampleInd = 0;
+  for (size_t textInd = 0; textInd < 2*text.size(); textInd++) {
+    if (text[textInd%text.size()] == sample[sampleInd]) {
       sampleInd++;
-    if (sampleInd == sample.size()-1)
-      return (textInd-(int)sample.size()+1)%(int)text.size();
+      if (sampleInd == sample.size()) {
+        return textInd-sample.size()+1;
+      }
+      continue;
+    }
+    if (sampleInd == 0) continue;
+    sampleInd = samplePrefixValues[sampleInd-1];
+    textInd--;
   }
   return -1;
 }
